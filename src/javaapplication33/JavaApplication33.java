e6861fa1068b4e6c8cfdb3080b13d298d9cdece3
johan <johan@johan-PC>
2016-11-06 21:10:17 -0500

    primer commit al repo

---
+++ src/javaapplication33/JavaApplication33.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication33;
+
+/**
+ *
+ * @author johan
+ */
+public class JavaApplication33 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        proyecto.getWindows();        
+    }
+}
